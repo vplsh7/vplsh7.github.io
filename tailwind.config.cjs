0c882fd565bae36c7f42bfc0f8b2ec84f5547053
vplsh7 <vplsh7@gmail.com>
2025-09-15 16:58:51 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,ts,jsx,tsx,md,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brand-dark": "#1e293b",
+        "brand-blue": "#3b82f6",
+        "moose-purple": "#8b5cf6"
+      }
+    }
+  },
+  plugins: []
+};
