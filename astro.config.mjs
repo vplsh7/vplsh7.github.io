c64b18fd1b1531d910c1630d8218d240fa76d9f0
vplsh7 <vplsh7@gmail.com>
2025-09-18 12:44:45 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://vplsh7.github.io",
   integrations: [tailwind(), mdx()],
 });
+
